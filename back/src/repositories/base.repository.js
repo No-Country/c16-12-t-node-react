dd65296b5fcb338ab028978ef77a4a15422de336
Paul Llanque <llanquepol@gmail.com>
2024-02-15 00:48:35 -0500

    [ADD] base, user repository

---
+++ back/src/repositories/base.repository.js
@@ -0,0 +1,27 @@
+let _model = null;
+
+export class BaseRepository {
+  constructor({ model }) {
+    _model = model;
+  }
+
+  async getAll({ page, limit }) {
+    throw new Error('Method not implemented.');
+  }
+
+  async getById(id) {
+    throw new Error('Method not implemented.');
+  }
+
+  async create(data) {
+    throw new Error('Method not implemented.');
+  }
+
+  async update(id, data) {
+    throw new Error('Method not implemented.');
+  }
+
+  async delete(id) {
+    throw new Error('Method not implemented.');
+  }
+}
