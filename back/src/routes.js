181c738126c37fcf2c1d655df4d8d6e8e027b840
Paul Llanque <llanquepol@gmail.com>
2024-02-15 01:01:04 -0500

    [ADD] config routes

---
+++ back/src/routes.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import compression from 'compression';
+
+import { notFoundMiddleware } from './middleware/index.js';
+import { AuthRoute } from './routes/index.js';
+
+/**
+ * Funtion to create express router
+ *
+ * @returns {express.Router} Configured express router
+ */
+
+export const routes = () => {
+  const router = express.Router();
+  const apiRoutes = express.Router();
+
+  const helleRoute = new AuthRoute();
+
+  router
+    .use(express.json())
+    .use(express.urlencoded({ extended: true }))
+    .use(cors())
+    .use(compression())
+    .use(helmet());
+
+  apiRoutes.use('/auth', helleRoute.routes());
+
+  router.use('/api', apiRoutes);
+  router.use(notFoundMiddleware);
+
+  return router;
+};
