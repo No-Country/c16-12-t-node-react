e2c6f30fbff9861b1a9bbb322607aedcab7af647
Paul Llanque <llanquepol@gmail.com>
2024-02-17 11:49:42 -0500

    elimicación de archivos config.js y modelos city y country

--- back/src/db/config.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from 'sequelize';
-import { envs } from '../config/index.js';
-
-const sequelize = new Sequelize(envs.BD_NAME, envs.DB_USERNAME, envs.DB_PASSWORD, {
-  host: envs.DB_HOST,
-  dialect: envs.DB_DIALECT_MYSQL,
-});
-
-export default sequelize;
