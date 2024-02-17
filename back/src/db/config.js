7c10f899b6e2071c9ee538f9ca24add93998b63c
Paul Llanque <llanquepol@gmail.com>
2024-02-17 09:04:44 -0500

    [ADD] variables de entorno para la conexión se base de datos

--- back/src/db/config.js
+++ back/src/db/config.js
@@ -1,8 +1,9 @@
 import { Sequelize } from 'sequelize';
+import { envs } from '../config/index.js';
 
-const sequelize = new Sequelize('nocountry', 'root', 'Pass@1234', {
-  host: 'localhost',
-  dialect: 'mysql',
+const sequelize = new Sequelize(envs.BD_NAME, envs.DB_USERNAME, envs.DB_PASSWORD, {
+  host: envs.DB_HOST,
+  dialect: envs.DB_DIALECT_MYSQL,
 });
 
 export default sequelize;
