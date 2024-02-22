803321c8ccfd31144d512501b91aa6ae002ac615
Paul Llanque <llanquepol@gmail.com>
2024-02-22 07:49:43 -0500

    [ADD] install react icons

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,9 +1,16 @@
+import { Avatar, StarRating } from '@/components/atoms';
+
 function App() {
+  const url =
+    'https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI';
+
   return (
     <>
       <h1>Hello World</h1>
+      <Avatar avatarUrl={url} userName="John Doe" size="small" />
+      <StarRating userRating={3} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
