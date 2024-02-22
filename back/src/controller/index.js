8dcabb6367531255b28d91cb794327378faa8a7f
Lucas Herrera <Lucasjherrera1551@gmail.com>
2024-02-22 17:49:05 -0300

    [FIX] Usage of barrels

--- back/src/controller/index.js
+++ back/src/controller/index.js
@@ -2,4 +2,4 @@ export * from './auth.controller.js';
 export * from './upload.controller.js';
 export * from './trip.controller.js';
 export * from './user.controller.js';
-
+export * from './cities.controller.js';
