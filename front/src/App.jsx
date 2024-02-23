0512509cccffd4d0041a75c92f44d9bae1123e52
Lucas Herrera <Lucasjherrera1551@gmail.com>
2024-02-23 15:54:25 -0300

    [ADD] adding botton component

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,12 +1,6 @@
-import { Button } from "./components/atoms/index";
 function App() {
   return (
     <>
-      <Button size='medium' content= 'register' />
-      <Button size='medium' content= 'login' />
-      <Button size='medium' content= 'confirmTravel' />
-      <Button size='medium' content= 'search' />
-      <Button size='small' content= 'viewTravel' />
       <h1>Hello World</h1>
     </>
   );
--- front/src/components/atoms/Button.jsx
+++ front/src/components/atoms/Button.jsx
@@ -2,8 +2,8 @@ import PropTypes from 'prop-types'
 
 export const Button = ({size, content}) => {
     const sizeVariants = {
-        small: 'bg-primary-200 w-48 h-12',
-        medium: 'bg-primary-500 w-96 h-12',
+        small: 'bg-primary-200 hover:bg-primary-300 w-48 h-12',
+        medium: 'bg-primary-300 hover:bg-primary-400 w-96 h-12',
         large: '',
     }
     const contentVariants = {
@@ -19,14 +19,11 @@ export const Button = ({size, content}) => {
     let buttonSize = sizeVariants[size]
 
     return (
-        <button type="submit" className={`m-5 font-inter text-xl rounded-md flex justify-center items-center ${buttonSize}`}>
+        <button type="submit" className={`m-5 text-xl rounded-md flex justify-center items-center ${buttonSize}`}>
         {buttonContent}
         </button>
     );
 };
-
-
-  
 
 Button.propTypes = {
     content: PropTypes.oneOf(['login', 'register', 'viewTravel', 'submit', 'search', 'confirmTravel']),
