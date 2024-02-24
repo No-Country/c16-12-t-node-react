376e873ca53384abf0078de0ebf63c8eb8f07fb1
Paul Llanque <llanquepol@gmail.com>
2024-02-23 21:35:11 -0500

    [UPDATE] delete country

--- back/src/repositories/country.respository.js
+++ back/src/repositories/country.respository.js
@@ -1,9 +1,21 @@
-import { CustomeError } from '../errors/index.js';
+import { CustomeError } from '../errors/custome.error.js';
 import { BaseRepository } from './base.repository.js';
 
 export class CountryRepository extends BaseRepository {
   constructor({ model }) {
     super({ model });
     this.model = model;
+  }
+
+  async deleteCountrById(id) {
+    try {
+      const country = await this.model.findOne({ where: { id } });
+      if (!country) {
+        return null;
+      }
+      return await this.model.destroy({ where: { id } });
+    } catch (error) {
+      throw CustomeError.serverError(`${error}`);
+    }
   }
 }
