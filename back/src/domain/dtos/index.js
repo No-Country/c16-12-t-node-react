c451b8f177236e6150da96a36fdfff8edf3a31b9
Paul Llanque <llanquepol@gmail.com>
2024-02-23 19:58:57 -0500

    [UPDATE] exports dto

--- back/src/domain/dtos/index.js
+++ back/src/domain/dtos/index.js
@@ -1,3 +1,5 @@
 export * from './auth/login.dto.js';
 export * from './auth/register-user.dto.js';
 export * from './user/update-user.dto.js';
+export * from './city/create-city.dto.js';
+export * from './city/update-city.dto.js';
