86e9fcdd4e3bd29a817f883081d96d193d2ad972
Paul Llanque <llanquepol@gmail.com>
2024-02-25 12:23:13 -0500

    [UPDATE] trip model with default values and adding priperties

--- back/src/db/mysql/models/trip.model.js
+++ back/src/db/mysql/models/trip.model.js
@@ -21,21 +21,34 @@ export const Trip = sequelize.define('Trip', {
     type: DataTypes.INTEGER,
     allowNull: false,
   },
-  seats_reserved: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
   seat_price: {
     type: DataTypes.FLOAT,
-    allowNull: false,
+    allowNull: true,
+    defaultValue: 0,
   },
   total_price: {
     type: DataTypes.FLOAT,
+    allowNull: true,
+    defaultValue: 0,
+  },
+  driver_id: {
+    type: DataTypes.INTEGER,
     allowNull: false,
+  },
+  trip_status: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: 'not_started',
+    validate: {
+      isIn: [['not_started', 'in_progress', 'completed']],
+    },
   },
 });
 
 Trip.belongsTo(City, { through: 'From_to', foreignKey: 'origin_id', targetKey: 'id' });
 Trip.belongsTo(City, { through: 'From_to', foreignKey: 'destiny_id', targetKey: 'id' });
-Trip.belongsTo(User, { through: 'Driver_Passenger', foreignKey: 'passenger_id', targetKey: 'id' });
-Trip.belongsTo(User, { through: 'Driver_Passenger', foreignKey: 'driver_id', targetKey: 'id' });
+Trip.belongsTo(User, {
+  foreignKey: 'driver_id',
+  targetKey: 'id',
+  allowNull: false,
+});
