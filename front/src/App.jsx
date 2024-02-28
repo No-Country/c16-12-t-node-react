feacb31a16411597178bf7733fee9d7a1c55e531
Santiago Conrero <santiconrero@hotmail.com>
2024-02-27 23:25:57 -0300

    commit

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { LoginComponent } from './components/molecules/LoginComponent';
 
@@ -6,13 +5,6 @@ const App = () => {
   return (
     <div className="App">
       <LoginComponent />
-=======
-import { RegisterForm } from "./components/molecules/index";
-function App() {
-  return (
-    <div className="flex justify-center items-center">
-      <RegisterForm/>
->>>>>>> 4bfbcf81bea0f8f935855f2ed80a4664efac0f4f
     </div>
   );
 };
