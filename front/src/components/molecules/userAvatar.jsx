0ccf00c4e8f450f542988a482ac1e01b5d599401
Paul Llanque <llanquepol@gmail.com>
2024-02-29 12:43:04 -0500

    [ADD] user avatar molecula component

---
+++ front/src/components/molecules/userAvatar.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+
+import { Avatar } from '../atoms/Avatar';
+import { StarRating } from '../atoms/StarRating';
+import { UserName } from '../atoms/Username';
+
+export const UserAvatar = ({ avatarUrl, username, rating, information }) => {
+  return (
+    <div className="flex items-top gap-4">
+      <Avatar avatarUrl={avatarUrl} userName={username} size="small" />
+      <div className="py-1 flex flex-col gap-4">
+        <UserName username={username} size="small" />
+        {information && <p className="text-gray-500">{information}</p>}
+        {rating && <StarRating userRating={rating} />}
+      </div>
+    </div>
+  );
+};
+
+UserAvatar.propTypes = {
+  id: PropTypes.number,
+  avatarUrl: PropTypes.string,
+  username: PropTypes.string,
+  rating: PropTypes.number,
+  information: PropTypes.string,
+};
