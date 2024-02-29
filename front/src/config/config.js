4f5764bb45f6f9750afca1b1998a2c7e49841abf
Paul Llanque <llanquepol@gmail.com>
2024-02-29 12:36:06 -0500

    [ADD] config environments

---
+++ front/src/config/config.js
@@ -0,0 +1,6 @@
+const TOKEN = import.meta.env.VITE_TOKEN;
+const USER_ID = import.meta.env.VITE_USER_ID;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const WS_ENDPOINT = import.meta.env.VITE_WS_ENDPOINT;
+
+export { TOKEN, USER_ID, API_BASE_URL, WS_ENDPOINT };
