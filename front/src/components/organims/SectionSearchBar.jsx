4fa116cecaec67b252e3855e2549035ecb0c2244
BombenDev <nicolasbomben@gmail.com>
2024-02-28 23:12:04 -0300

    [ADD] section search bar It is still in development

--- front/src/components/organims/SectionSearchBar.jsx
+++ front/src/components/organims/SectionSearchBar.jsx
@@ -1,22 +1,22 @@
-import  { SearchBar } from '@/components/atoms/index';
+import { SearchBar } from '@/components/atoms/index';
 import iconoLanding from '@/assets/img/iconoLanding.png';
 
 export const SectionSearchBar = () => {
   return (
-    <section className="w-full h-full">
-      <div className="block py-24 mx-16">
+    <section className="container">
+      <div className="block pt-24 ml-20">
         <h1 className="text-6xl font-extrabold leading-normal tracking-wide drop-shadow-md">
           Ahora viajAR es
           <br /> mas fácil,barato
           <br />y seguro!
         </h1>
       </div>
-      <div className="flex justify-between items-center mx-16">
-        <div>
+      <div className="flex justify-center sm:flex-col lg:flex-row lg:ml-20 mt-20">
+        <div className="drop-shadow-md">
           <SearchBar />
         </div>
-        <div>
-          <img src={iconoLanding} alt="iconoLanding"/>
+        <div className='flex pt-5'>
+          <img src={iconoLanding} alt="iconoLanding" />
         </div>
       </div>
     </section>
