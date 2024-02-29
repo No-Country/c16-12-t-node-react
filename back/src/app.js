df2a0c415dd1b13da3e504485249555ac9ef049c
Paul Llanque <llanquepol@gmail.com>
2024-02-29 03:00:07 -0500

    [ADD] websocket configuracion and recive message from client

--- back/src/app.js
+++ back/src/app.js
@@ -1,23 +1,32 @@
+import express from 'express';
+import { WebSocketServer } from 'ws';
+
 import { envs } from './config/index.js';
-import { Server } from './server.js';
 import { routes } from './routes.js';
 import { sequelize } from './db/mysql/config/config.connection.js';
 
 import './db/mysql/models/index.js';
+import { webSocketServer } from './webSocket/ws.js';
 
 (async () => {
-  const { BASE_API, PORT } = envs;
   try {
     await sequelize.sync({ alter: false });
-    console.log('Connected to DB successfully');
-
-    const route = routes(BASE_API);
-    const server = new Server({ port: PORT, router: route });
-    const port = await server.start();
-    if (port) {
-      console.log(`Server running on port ${port}`);
-    }
+    await main();
   } catch (error) {
-    console.error(`Something went wrong: ${error}`);
+    console.log(error);
   }
 })();
+
+async function main() {
+  const { BASE_API, PORT } = envs;
+
+  const app = express();
+  app.use(routes(BASE_API));
+
+  const server = app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+  });
+
+  const wsServer = new WebSocketServer({ server });
+  webSocketServer(wsServer);
+}
