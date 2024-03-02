189464cad5aee936984830102743d5bc1740e9ad
Paul Llanque <llanquepol@gmail.com>
2024-03-01 23:26:45 -0500

    [ADD] login page

---
+++ front/src/pages/LoginPage.jsx
@@ -0,0 +1,10 @@
+import { LoginComponent } from '@/components/molecules/LoginComponent';
+import { LayOut } from '@/components/templates/template';
+
+export const LoginPage = () => {
+  return (
+    <LayOut>
+      <LoginComponent />
+    </LayOut>
+  );
+};
