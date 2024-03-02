ae0f9a75c45e26b644b6235f29a204c7a2e6aaed
Paul Llanque <llanquepol@gmail.com>
2024-03-02 17:22:25 -0500

    [ADD] Error not result section

--- front/src/components/templates/Section.jsx
+++ front/src/components/templates/Section.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
 export const Section = ({ children }) => {
-  return <section className="my-16">{children}</section>;
+  return <div className="my-16">{children}</div>;
 };
 
 Section.propTypes = {
