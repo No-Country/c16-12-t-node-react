fc565cb92a2f44c81cdab4c1ddab3c366fae8f7e
Paul Llanque <llanquepol@gmail.com>
2024-03-02 10:56:41 -0500

    [UPDATE] template

--- front/src/components/templates/template.jsx
+++ front/src/components/templates/template.jsx
@@ -4,13 +4,13 @@ import Footer from '../organims/Footer';
 
 export const LayOut = ({ children }) => {
   return (
-    <div>
+    <>
       <Header />
       <div className="w-full min-h-[calc(100vh-300px)] px-1.5 md:container mx-auto py-9">
         {children}
       </div>
       <Footer />
-    </div>
+    </>
   );
 };
 
