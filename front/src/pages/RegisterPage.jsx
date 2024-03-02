708196a35e1a02916ad85944e26591f8b91a506a
Paul Llanque <llanquepol@gmail.com>
2024-03-01 23:26:28 -0500

    [ADD] register page

---
+++ front/src/pages/RegisterPage.jsx
@@ -0,0 +1,10 @@
+import { RegisterForm } from '@/components/molecules/RegisterForm';
+import { LayOut } from '@/components/templates/template';
+
+export const RegistePage = () => {
+  return (
+    <LayOut>
+      <RegisterForm />
+    </LayOut>
+  );
+};
