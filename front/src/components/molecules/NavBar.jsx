08ecf96b2801e176d64386a22b86ebe9b68981c4
Paul Llanque <llanquepol@gmail.com>
2024-03-01 21:39:12 -0500

    [REFACTOR] navBar

--- front/src/components/molecules/NavBar.jsx
+++ front/src/components/molecules/NavBar.jsx
@@ -1,42 +1,83 @@
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
-import { Button } from '../atoms/index.js';
-import { MdMenu } from 'react-icons/md';
-import Logo from '../../assets/img/logo.png';
-export const NavBar = () => {
+import PropTypes from 'prop-types';
 
-  const [showMenu, setShowMenu] = useState(false);
+import { Avatar } from '../atoms/Avatar';
 
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
-  };
+export const NavBar = ({ isClicked }) => {
+  // todo: check if user is logged
+  const user = true;
+
+  const remplate = (
+    <div className="md:hidden">
+      <nav className="flex flex-col py-4 px-2 absolute top-16 left-0 w-full bg-[#e5e5e5]">
+        <Link
+          to="/"
+          className="text-black hover:bg-primary-300 hover:text-black rounded-lg py-1.5 px-3 block"
+        >
+          Inicio
+        </Link>
+        <Link
+          to="/about"
+          className="text-black hover:bg-primary-300 hover:text-black rounded-lg py-1.5 px-3"
+        >
+          Sobre Nosotros
+        </Link>
+        <Link
+          to="/contact"
+          className="text-black hover:bg-primary-300 hover:text-black rounded-lg py-1.5 px-3"
+        >
+          Contacto
+        </Link>
+      </nav>
+    </div>
+  );
 
   return (
-    <nav className='flex flex-col sm:flex-row md:flex-row justify-between items-center h-20 mx-8'>
-        {/* logo */}
-        <div>
-          <Link to="/">
-            <img src={Logo} alt="Logo" className='h-14' />
-          </Link>
-        </div>
-        
-        {/* menu desplegable */}
-        <div className='block lg:hidden md:hidden' >
-          <button onClick={toggleMenu} className="text-white hover:text-gray-300 focus:outline-none">
-            <MdMenu size={24} />
-          </button>
-        </div>
-
-        {/* menu principal */}
-        <div className={`flex items-center ${showMenu ? 'block' : 'hidden'} lg:block lg:relative lg:bg-transparent lg:p-0 lg:space-x-0`}>
-          <Link to="/" className="hover:bg-primary-300 inline-block px-4 py-2 rounded-lg">Home</Link>
-          <Link to="/" className="hover:bg-primary-300 inline-block px-4 py-2 rounded-lg">Sobre nosotros</Link>
-          <Link to="/" className="hover:bg-primary-300 inline-block px-4 py-2 rounded-lg">Contactanos</Link>
-        </div>
-      {/* luego de inicar la sesion debe renderizar el avatar.*/}
-        <Button content="login" size="small" color="primary_normal">
-          <Link to="/login"></Link>
-        </Button>
-    </nav>
+    <div className="flex justify-end items-center h-16 w-full gap-2">
+      <nav className="hidden md:flex md:gap-2">
+        <Link
+          to="/"
+          className="text-black hover:bg-primary-300 hover:text-black rounded-lg py-1.5 px-3 block"
+        >
+          Inicio
+        </Link>
+        <Link
+          to="/about"
+          className="text-black hover:bg-primary-300 hover:text-black rounded-lg py-1.5 px-3"
+        >
+          Sobre Nosotros
+        </Link>
+        <Link
+          to="/contact"
+          className="text-black hover:bg-primary-300 hover:text-black rounded-lg py-1.5 px-3"
+        >
+          Contacto
+        </Link>
+      </nav>
+      {isClicked && remplate}
+      <div className="flex gap-2 justify-end">
+        {user && <Avatar size="mini" />}
+        {!user && (
+          <>
+            <Link
+              to="/login"
+              className="bg-primary-400 text-white hover:bg-primary-600 rounded-lg py-1.5 px-3"
+            >
+              Ingresar
+            </Link>
+            <Link
+              to="/register"
+              className="bg-primary-400 text-white hover:bg-primary-600 rounded-lg py-1.5 px-3"
+            >
+              Registrarse
+            </Link>
+          </>
+        )}
+      </div>
+    </div>
   );
-}
+};
+
+NavBar.propTypes = {
+  isClicked: PropTypes.bool,
+};
