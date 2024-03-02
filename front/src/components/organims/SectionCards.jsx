b9bb4fb9ec5b73089aef67e465de1015f7f79404
Paul Llanque <llanquepol@gmail.com>
2024-03-02 10:54:40 -0500

    [UPDATE] card's style

--- front/src/components/organims/SectionCards.jsx
+++ front/src/components/organims/SectionCards.jsx
@@ -73,10 +73,10 @@ export const SectionCards = () => {
 
   return (
     <div className="container mx-auto px-4 py-8 flex flex-col justify-center items-center">
-      <p className="text-xl md:text-2xl font-bold text-gray-800 mb-10">
+      <p className="text-xl md:text-left md:text-4xl font-bold text-gray-800 mb-10">
         Descubre los viajes disponibles
       </p>
-      <div className="flex flex-wrap justify-center gap-6">
+      <div className="flex flex-wrap justify-center gap-x-20 gap-y-14">
         {trips.map((trip) => (
           <CardComponent
             key={trip.id}
