444850aa716cd9a60862923cfe85fb76eab314d4
Paul Llanque <llanquepol@gmail.com>
2024-03-01 23:17:39 -0500

    [UPDATE] header style

--- front/src/components/organims/Header.jsx
+++ front/src/components/organims/Header.jsx
@@ -15,7 +15,7 @@ export const Header = () => {
 
   return (
     <header className="w-full py-2">
-      <div className="flex px-1.5 justify-between items-center md:container mx-auto relative">
+      <div className="flex px-1.5 justify-between gap-1.5 items-center md:container mx-auto relative">
         <Link to="/" className="inline-block">
           <img src={Logo} alt="Logo" className="object-cover" />
         </Link>
