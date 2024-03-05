7af7d92eb28bd3d8897a26187f0394111e397f8c
Paul Llanque <llanquepol@gmail.com>
2024-03-05 11:06:52 -0500

    [UPDATE] remove require props validation

--- front/src/components/atoms/DataContent.jsx
+++ front/src/components/atoms/DataContent.jsx
@@ -4,14 +4,14 @@ export const DataContent = ({ icon, aperture, description }) => (
   <div className="flex justify-start items-center space-x-2">
     <span>{icon}</span>
     <p className="text-lg text-gray-600 space-x-2">
-      <span className="font-semibold">{aperture}</span>
+      {aperture && <span className="font-semibold">{aperture}</span>}
       <span className="font-regular">{description}</span>
     </p>
   </div>
 );
 
 DataContent.propTypes = {
-  icon: PropTypes.node.isRequired,
-  aperture: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
+  icon: PropTypes.node,
+  aperture: PropTypes.string,
+  description: PropTypes.string,
 };
